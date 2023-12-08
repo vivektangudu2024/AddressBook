651625f7a40c4a34e6bfeb054753bf9a1cd02c0b
vivektangudu2024 <tanguduvivek2003.com>
2023-12-08 22:40:11 +0530

    Welcome Message

---
+++ adresssBook.java
@@ -0,0 +1,20 @@
+package com.day5;
+
+import java.util.Scanner;
+
+public class adresssBook {
+
+    public static void main(String[] args) {
+        // Displaying a welcome message
+        displayWelcomeMessage();
+
+
+
+        // TODO: Add more functionality as needed
+    }
+
+    // Method to display a welcome message
+    private static void displayWelcomeMessage() {
+        System.out.println("Welcome to Address Book Program!");
+    }
+}
